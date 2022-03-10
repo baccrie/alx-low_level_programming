570fd25f123ab33f58b7b5b36cfe56794bcb500e
baccrie <bakarerilwan1@gmail.com>
2022-03-10 13:31:15 -0800

    Corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int integ;
 long int lon;
 long long int longg;
 float floa;
-printf("Size of a char: %c byte(s)", sizeof(charac));
-printf("Size of a int:  %d byte(s)", sizeof(integ));
-printf("Size of a long int: %d byte(s)", sizeof(lon));
-printf("Size of a long long int: %d byte(s)", sizeof(longg));
-printf("Size of a float: %f byte(s)", sizeof(floa));
+printf("Size of a char: %c byte(s)\n", sizeof(charac));
+printf("Size of a int:  %d byte(s)\n", sizeof(integ));
+printf("Size of a long int: %d byte(s)\n", sizeof(lon));
+printf("Size of a long long int: %d byte(s\n)", sizeof(longg));
+printf("Size of a float: %f byte(s\n)", sizeof(floa));
 return (0);
 }
