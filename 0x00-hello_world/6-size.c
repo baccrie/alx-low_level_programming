5dec6c131306f00c8891a6ab20333332b324b236
baccrie <bakarerilwan1@gmail.com>
2022-03-10 13:35:59 -0800

    My grit is overflowing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char charac;
-int integ;
-long int lon;
-long long int longg;
-float floa;
-printf("Size of a char: %c byte(s)\n", sizeof(charac));
-printf("Size of a int:  %d byte(s)\n", sizeof(integ));
-printf("Size of a long int: %d byte(s)\n", sizeof(lon));
-printf("Size of a long long int: %d byte(s\n)", sizeof(longg));
-printf("Size of a float: %f byte(s\n)", sizeof(floa));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
