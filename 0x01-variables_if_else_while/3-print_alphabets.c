0094ad081c314db9d90915f004d23e528e99a90e
baccrie <bakarerilwan1@gmail.com>
2022-03-11 11:42:49 -0800

    Taak 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,23 @@
-#include <stdio.h>                          /**
- * main - Entry - point
+#include <stdio.h>
+/**
+ * main - Entry point
  * Task 3. alphABET
- * This c script is written by baccrie © 20
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char lowercase; /* Declaring a variable for the lower case alphabets*/
+char lowercase; /* Declaring a variable for the lower case alphabets*/
 
-        for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-        {
-                putchar(lowercase);
-	}
-	char uppercase; /* Declaring a varia
-ble for the upper case alphabets*/
-                                                    for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)                                     {                                                   putchar(uppercase);
-        }
-        putchar('\n');
-        return (0);
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+	char uppercase;
+
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+{
+putchar(uppercase);
+}
+putchar('\n');
+return (0);
 }
