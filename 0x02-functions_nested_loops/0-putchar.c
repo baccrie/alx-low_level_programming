33a54006aacbbe099620b7f7faac4092abced8f4
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-15 12:26:56 +0100

    Task 0.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
  * main - Entry point
- *
+ * The main.h is an header file that has th
+ * the prototyping definition of the
+ * _putchar function
  * Return: Always 0 (Success)
  */
 int main(void)
