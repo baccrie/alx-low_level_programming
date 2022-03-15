09b8d650c1accf74e2fa5a81ca0e0cee5e843263
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-15 16:07:56 +0100

    Task 2.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * The main.h is an header file that has th
+ * the prototyping definition of the
+ * _putchar function
+ * copyright © baccrie 2022
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* defining the alphabet vafiable */
+char alphabets;
+
+for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+{
+_putchar(alphabets);
+}
+_putchar('\n');
+}
+
