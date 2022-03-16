af1b190d13b4d98a7b8ddb2ab639f71736220c10
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-16 23:40:11 +0100

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
 /**
-* main - tests function that prints if
-* integer is positive or negative
-* baccrie © 2022
+* main - main
 * Return: 0
 */
 
 int main(void)
 {
-int i;
 
+int i;
 i = 0;
+
 positive_or_negative(i);
+
 return (0);
 }
