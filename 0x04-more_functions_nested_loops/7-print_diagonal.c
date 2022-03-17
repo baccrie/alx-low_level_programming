36b83d368ce49eb831aac47697747f7641907f22
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-17 22:50:18 +0100

    Mandatory Task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,34 @@
 #include "main.h"
 /**
- * print_diagonal - check the code
- * @n:
- * No return value
+ * print_diagonal - prints diagonal line
+ * @n: print int
+ * Return: Always 0.
  */
 void print_diagonal(int n)
 {
-	int count = 0;
 
-	while (count < n)
+/*Delcaring statements*/
+	int i;
+	int z;
+
+	if (n > 0)
 	{
-		_putchar(92);
-		_putchar('\n');
-		_putchar(' ');
-		count++;
+		for (i = 0; i < n; i++) /*Print (_) n times*/
+		{
+			for (z = 0; z < i ; z++) /*Print (_) * Second IF*/
+			{
+				_putchar (' ');
+			} /*End second IF*/
+			_putchar (92);
+			_putchar ('\n'); /*New line*/
+
+		}
+
 	}
-	return;
+
+	else
+	{
+		_putchar ('\n');
+	}
+
 }
