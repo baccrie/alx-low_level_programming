aad13e36b4c222a2366f67b8c4f5dde28b2241bf
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-17 18:02:48 +0100

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - void
+ * @num: prints char
+ * copyright © baccrie 2022
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	char num = '47';
+
+	while (num <= '58')
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
+
