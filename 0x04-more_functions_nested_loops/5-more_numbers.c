457bbab71f6efbee3d4bf3faebdd17df5b4359f3
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-17 20:45:02 +0100

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers
+ *
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int num;
+	int count;
+
+	count = 1;
+	while (count <= 10)
+	{
+		num = 0;
+		while(num < 15)
+		{
+			_putchar(num);
+			++num;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
