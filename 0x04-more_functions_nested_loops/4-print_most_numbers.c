973b7776b5f71debda752dc2b8963ff8362ddf9d
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-17 18:33:19 +0100

    Task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_most_numbers - void
+ *
+ * copyright © baccrie 2022
+ * Return: Always 0
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	while ( num <= 9)
+	{
+		if ( num == 2)
+		{
+			continue;
+		}
+		if ( num == 4 )
+		{
+			continue;
+		}
+		_putchar(num);
+		++num;
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/me.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int num = 48;
+
+	{
+		while ( num <= 57 )
+		{
+			putchar(num);
+			num++;
+		}
+	}
+}
