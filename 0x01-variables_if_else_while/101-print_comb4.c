c345885a61427715202f2c9d4299cf05f9f99334
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-19 17:49:27 +0100

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,35 +11,35 @@
 
 int main()
 {
-        /* Declaring variable for the for loops*/
-        int left;
-        int middle;
-        int right;
+	/* Declaring variable for the for loops*/
+	int left;
+	int middle;
+	int right;
 
-        /* Outer loop */
-        for (left = 48; left <= 55; left++)
-        {
-                /* middle loop */
-                middle = left + 1;
-                while (middle <= 56)
-                {
-                        /* most inner loop */
-                        for (right = middle + 1; right <= 57; ++right)
-                        {
-                                putchar(left);
-                                putchar(middle);
-                                putchar(right);
-                                if (left == 55 && middle == 56 && right == 57)
-                                {
-                                        break;
-                                }
-                                putchar(',');
-                                putchar(' ');
-                        }
-                        middle++;
-                }
-        }
-        putchar('\n');
-        return (0);
-        /* Copyright © 2022 */
+	/* Outer loop */
+	for (left = 48; left <= 55; left++)
+	{
+		/* middle loop */
+		middle = left + 1;
+		while (middle <= 56)
+		{
+			/* most inner loop */
+			for (right = middle + 1; right <= 57; ++right)
+			{
+				putchar(left);
+				putchar(middle);
+				putchar(right);
+				if (left == 55 && middle == 56 && right == 57)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			middle++;
+		}
+	}
+	putchar('\n');
+	return (0);
+	/* Copyright © 2022 */
 }
