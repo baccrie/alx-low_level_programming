7be211e22a558c40252408e750a0d0bb4121a1cc
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-19 13:27:41 +0100

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Task 3. alphABET
+ * Task 5. Numbers
+ * Copyright © 2022 baccrie
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int numb;
-numb = 0;
-while (numb < 10)
-{
-printf("%d", numb);
-++numb;
-}
-putchar('\n');
-return (0);
+	int numb; /* Declare variable for loop */
+	numb = 0;
+	while (numb < 10) /* loops */
+	{
+		printf("%d", numb);
+		++numb;
+	}
+	putchar('\n');
+	return (0);
+	/* Copyright © 2022 baccrie */
 }
