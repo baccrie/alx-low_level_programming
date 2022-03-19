a853305364649c84b9115cc6526f86c80ab28529
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-19 13:56:42 +0100

    Task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,17 +2,18 @@
 /**
  * main - Entry - point
  * Task 7. sometimes suffer from insomnia.
- *
+ * Copyright © 2022 baccrie
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char alphabet; /*declaring variable*/
+	char alphabet; /*declaring variable */
 
-for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
-{
-putchar(alphabet);
-}
-putchar('\n');
-return (0);
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+	/* Copyright © 2022 baccrie */
 }
