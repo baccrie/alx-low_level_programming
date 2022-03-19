b641d29072aa71d61c2e173b63ef513a92ffdfcf
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-19 13:46:35 +0100

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Task 6. alphABET
+ * Task 6. Numberz
+ * Copyright © 2022 baccrie
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int numb = 48;
+	int numb;/* Declare variable for loop */
 
-while (numb <= 57)
-{
-putchar(numb);
-++numb;
-}
-putchar('\n');
-return (0);
+	for (numb = 48; numb < 58; numb++)
+	{
+		putchar(numb);
+	}
+	putchar('\n');
+	return (0);
+	/* Copyright © 2022 baccrie */
 }
