4664deca4ddb8a3811212b4bb2302e8e60122f97
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-19 13:22:29 +0100

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Task 3. alphABET
+ * Task 4. When I was having that alphabet
+ * soup, I never thought that it would
+ * pay off
+ * Copyright © 2022 baccrie
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char letters;
-for (letters = 'a'; letters <= 'z'; letters++)
-{
-if (letters == 'e')
-{
-continue;
-}
-if (letters == 'q')
-{
-continue;
-}
-putchar(letters);
-}
-putchar('\n');
-return (0);
+	char letters;/* Declare variables */
+
+	/* for loop */
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		/* if statement to exclude letter e and q */
+		if (letters == 'e' || letters == 'q')
+		{
+			continue;
+		}
+		putchar(letters);
+	}
+	putchar('\n');
+	return (0);
+	/* Copyright 2022 © baccrie */
 }
