78fc2d1f18a78896cb3eed5643e6f6a0009d8751
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-19 11:46:05 +0100

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)/* If statement starts here for positive n */
 	{
-		printf("%d is greater than 0\n", n);
+		printf("%d is positive", n);
 	}
-	else if(n == 0)/* if else for n is zero */
+	else if (n == 0)/* if else for n is zero */
 	{
-			printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else/* if statement for negative n */
 	{
-				printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 
 	return (0);
