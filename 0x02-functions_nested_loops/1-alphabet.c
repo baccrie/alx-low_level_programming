2ea9f030c3f2bdaff6d8833bc18dce7f2fe37984
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-20 18:32:10 +0100

    arrange codes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,22 +1,25 @@
 #include "main.h"
 /**
  * print_alphabet - print alphabet
- * lowercase
+ * Task 1. I sometimes suffer from insomnia.
+ * And when I can't fall asleep,
+ * I play what I call the alphabet game.
+ *
  * The main.h is an header file that has th
  * the prototyping definition of the
- * _putchar function
  * copyright © baccrie 2022
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
-/* defining the alphabet vafiable */
-char alphabets;
+	/* declaring the alphabet variable */
+	char alphabets;
 
-for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
-{
-_putchar(alphabets);
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		_putchar(alphabets);
+	}
+	_putchar('\n');
+	/* copyright © 2022 baccrie */
 }
-_putchar('\n');
-}
-
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,24 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - print alphabet
- * Copyright baccrie © 2022
- * All right reserved
+ * Task 2. 10 x alphabet
+ * Cooyright © 2022 baccrie
  * Return: always 0
  */
+
 void print_alphabet_x10(void)
 {
-int alphabet; /*Declaring variable*/
-int cycle;
+	int alphabet; /*Declaring variable*/
+	int cycle;
 
-cycle = 0;
-while (cycle < 10) /*While loop */
-{
-for (alphabet = 'a' ; alphabet <= 'z'; alphabet++) /*for loop*/
-{
-_putchar(alphabet);
-}
-cycle++;
-_putchar('\n');
-}
+	cycle = 0;
+	while (cycle < 10) /*While loop */
+	{
+		for (alphabet = 'a' ; alphabet <= 'z'; alphabet++) /*for loop*/
+		{
+			_putchar(alphabet);
+		}
+		cycle++;
+		_putchar('\n');
+	}
 }
