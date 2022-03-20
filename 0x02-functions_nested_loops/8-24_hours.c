2c99ceff2da51c8986666031b7564ef8b62df2d9
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-20 23:10:16 +0100

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -49,6 +49,8 @@ void jack_bauer(void)
 		}
 		first++;
 	}
-	_putchar('\n');
+	/* i love watching 24 hours movie but it took me a whole */
+	/* 24 hours to write a script that prints 00:00 to 23:59 */
+	/* I wont forget crazy characters like chloe, Christopher Henderson, Nina myers, Tony Almeida, Ramon Salazar, Preaident Palmer, Stephen Saunders, And finally the king Jack Bauer */
 	/* copyright © 2022 baccrie */
 }
