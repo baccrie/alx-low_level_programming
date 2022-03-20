a2b111f0ba460597c196ea4e55ef02a43f44f2c9
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-20 19:40:44 +0100

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
- * _islower - return 0 letter not lowercase
- * 1 letter lowercase
+ * _islower - function that checks for lowercase character
  * baccrie © 2022 all right reserved
- * @c: The int to print
+ * @c: The int to use
  * Return: 0 not lowercase, 1 lowercase
  */
+
 int _islower(int c)
 
 {
-
-if (c > 'a' && c < 'z')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	/* Copyright 2022 © baccrie */
 }
