17a71753f1d9dc977e033a327cab61306353019c
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-21 13:35:06 +0100

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * swap_int - (Task 1)unction that swaps the values of two integers.
+ * @a: - an int
+ * @b: - an int
+ * Copyright © 2022 baccrie
+ * Return: Either 0 or 1
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+	/* Copyright © 2022 baccrie */
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -4,3 +4,6 @@
 ## •Mandatory tasks files  and what they do
 ###     0-reset_to_98.c
 A function that takes a pointer to an int as parameter and updates the value it points to to 98.
+
+###	1-swap.c
+function that swaps the values of two integers.
