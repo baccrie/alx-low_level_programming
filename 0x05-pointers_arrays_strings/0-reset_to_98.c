a238a58a04188d9d6f20e8a76426923849e6867d
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-21 13:09:40 +0100

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as
  * parameter and updates the value it points to to 98
- * @*n - A pointer
+ * @n - A pointer
  * Copyright 2022 © baccrie
  * Return: Either 0 or 1
  */
@@ -10,4 +10,5 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
+	/* Copyright © 2022 baccrie */
 }
