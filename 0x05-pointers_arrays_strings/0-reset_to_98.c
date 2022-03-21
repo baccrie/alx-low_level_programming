1dfde97cff5e537b499a764535a771d6404bf26e
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-21 13:04:08 +0100

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - takes a pointer to an int as
+ * parameter and updates the value it points to to 98
+ * @*n - A pointer
+ * Copyright 2022 © baccrie
+ * Return: Either 0 or 1
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,6 @@
+## 0x05-pointers_arrays_strings
+
+
+## •Mandatory tasks files  and what they do
+###     0-reset_to_98.c
+A function that takes a pointer to an int as parameter and updates the value it points to to 98.
