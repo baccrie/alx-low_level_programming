49f892de7d3d54f4757a12d7d8181f25ae6beff3
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-22 21:33:46 +0100

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,24 +8,18 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
+	int sum;
 	int i;
 
-	sum3 = 0;
-	sum5 = 0;
 	sum = 0;
-
-	for (i = 0; i < 1024; ++i)
+	int = 1;
+	while (i < 1024)
 	{
-		if ((i % 3) == 0)
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
+			sum = sum + i;
 		}
+		++i;
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
-	return (0);
+	putchar('\n');
 }
