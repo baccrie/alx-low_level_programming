237b17c7767cc0ae64cf0f29711dc97e722e76da
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-22 22:53:21 +0100

    Task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the sum of the
- * even-valued terms
+ * main - prints the first 50 Fibonacci numb
+ * ers, starting with 1 and 2
  * followed by a new line
+ * Copyright © 2022 baccrie
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+	/* Declaring the variables using unsigned long because */
+	/* it has more memory and can accommodate the big fibonnaci numbers */
+	unsigned long int sum, sum1, sum2, evenSum;
+	int count;
 
-	j = 1;
-	k = 2;
+	evenSum = 0;
 	sum = 0;
-
-	for (i = 1; i <= 33; ++i)
+	sum2 = 1;
+	sum1 = 2;
+	while (sum <= 4000000)
 	{
-		if (j < 4000000 && (j % 2) == 0)
+		
+		sum = sum1 + sum2;
+		if ((sum % 2) == 0)
 		{
-			sum = sum + j;
+			evenSum = evenSum + sum;
 		}
-		next = j + k;
-		j = k;
-		k = next;
+		sum2 = sum1;
+		sum1 = sum;
 	}
-
-	printf("%lu\n", sum);
-
+	putchar('\n');
 	return (0);
+	/*.Copyright © baccrie 2022 */
 }
