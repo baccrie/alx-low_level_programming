d65164c41fee6e48327af0d1730520322867229e
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-23 09:08:36 +0100

    Find the bug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
 * main - causes an infinite loop
-* Debugging Task 1
 * Copyright © 2022 baccrie
 * Return: 0
 */
@@ -11,11 +10,16 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-	/*while (i < 10)
-	{
-		putchar(i);
-	}*/
+
+	/*
+	 *while (i < 10)
+	 *{
+	 *Infinite loop - No increase of variable
+	 *putchar(i);
+	 }
+	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 	/* Copyright © 2022 baccrie */
