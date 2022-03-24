0a48d65aa79bada30b9599d3081933a2716d41e3
baccrie <bakarerilwan1@gmail.comgit config color.ui autogit configgit config -lgit helpconfig helpgit config --global user.email bakarerilwan1@gmail.com>
2022-03-24 18:44:31 +0100

    Fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,6 @@
  */
 
 char *_strncat(char *dest, char *src, int n)
-
 {
 	int i, j;
 
@@ -17,7 +16,6 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
