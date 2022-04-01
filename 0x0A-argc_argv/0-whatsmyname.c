c443fb0f8150fa5dc0c92df60e146539663f8374
baccrie <bakarerilwan1@gmail.com>
2022-04-01 17:26:05 +0100

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * @argv - the no of command line arguement
- * @argc[] - a pointer to the arguement
- * Return: 0 or 1
+ * @argc - a pointer to the arguement
+ * Return: 0
  * Copyright © 2022 baccrie
  */
 
 int main(int argv, char *argc[])
 {
 	printf("%s\n", argc[0]);
+	return (0);
 	/* Copyright © 2022 baccrie */
 }
