c9cbec150fc067bd85d426076c908c3e53ac428c
baccrie <bakarerilwan1@gmail.com>
2022-04-01 18:53:21 +0100

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
 /**
- * main - Entry point
- * @argc - no of command line arguement including filename
- * @argv[] - pointer to arguement
- * Return: 0 or 1
- * Copyright © 2022 baccrie
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+
+int check_num(char *str)
+
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
-{
-	int *a;
-	int i;
-	int sum;
 
-	if (argc <= 1)
+{
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
 	{
-		printf("0\n");
-	}
-	//else if ();
-	else if (argc > 1)
-	{
-		i = 0;
-		sum = 0;
-		while (i < argc)
+		if (check_num(argv[count]))
+
 		{
-			a[i] = atoi(argv[i]);
-			if (isalpha(argv[i]))
-			{
-				sum = '\0';
-				printf("Error\n");
-				return (1);
-			}
-			sum = sum + a[i];
-			i++;
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+			sum += str_to_int;
 		}
-		printf("%d\n", sum);
+
+
+		/*Condition If one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
 	}
+
+	printf("%d\n", sum); /*print sum*/
+
+	return (0);
 }
