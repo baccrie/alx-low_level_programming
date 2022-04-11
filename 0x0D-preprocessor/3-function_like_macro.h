cb15c73eab2a87ca47b2bf07fa1f4d113a029f3d
baccrie <bakarerilwan1@gmail.com>
2022-04-11 17:38:48 +0100

    Fixed

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
+#ifndef BACCRIE
+#define BACCRIE
 #define ABS(x) ((x < 0) ? -(x) : (x))
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,3 +1,4 @@
-#ifndef SUM
+#ifndef BACCRIE
+#define BACCRIE
 #define SUM(x, y) ((x) + (y))
 #endif
