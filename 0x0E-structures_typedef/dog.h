b846d80ba82c394697d1378647cd01bed74ef0ed
baccrie <bakarerilwan1@gmail.com>
2022-04-11 13:00:19 +0100

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * struct dog - 0.Poppy
+ * @name: memeber thats holds name
+ * @age: member that holds age
+ * @owner: memeber that holds owner
+ * Description: a structure with three elements
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+	/* Copyright © 2022 baccrie */
+}
