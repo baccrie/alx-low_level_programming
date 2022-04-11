3ed93d81561cb9159758bb23f28b1073d05c8ec7
baccrie <bakarerilwan1@gmail.com>
2022-04-11 21:05:25 +0100

    Tired of Alx

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,40 +2,23 @@
 #include <stdio.h>
 
 /**
- * print_dog - print dog properties
- * @d: update later
- * Description: later
+ * print_dog - prints a dog
+ *
+ * @d: dog to print
+ *
  * Return: void
  */
-
 void print_dog(struct dog *d)
 {
 	if (d == 0)
-	{
 		return;
-	}
 	if (d->name == 0)
-	{
-		printf("Name; (nil)\n");
-	}
+		printf("Name: (nil)\n");
 	else
-	{
 		printf("Name: %s\n", d->name);
-	}
-	if (d->age == 0)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %f\n", d->age);
-	}
+	printf("Age: %f\n", d->age);
 	if (d->owner == 0)
-	{
 		printf("Owner: (nil)\n");
-	}
 	else
-	{
 		printf("Owner: %s\n", d->owner);
-	}
 }
