f64f6a051f57eb71aeafaf875ef41a942477ec4f
baccrie <bakarerilwan1@gmail.com>
2022-04-11 20:53:01 +0100

    Fixed

--- 0x0E-structures_typedef/1_init_dog.c
+++ 0x0E-structures_typedef/1_init_dog.c
@@ -2,24 +2,23 @@
 
 /**
  * init_dog - a function
- * @d: a struct variable
- * @name: a member
- * @age: member
- * @owner: member
- * Description: will update later
- * Return: void
+ * @d: pointer
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Description: later
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{
 		return;
-	}
+
 	else
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
+	/* Copyright © 2022 baccrie */
 }
