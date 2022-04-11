21296763879b637c6e085f6e8ff26fdfb0155799
baccrie <bakarerilwan1@gmail.com>
2022-04-11 20:58:37 +0100

    Nonzense

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,12 +1,13 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- * init_dog - a function
- * @d: pointer
- * @name: name
- * @age: age
- * @owner: owner
- * Description: later
+ * init_dog - initialize a variable of type struct dog
+ *@d: array
+ *@name: name
+ *@age: age
+ *@owner: owner
+ * Description: This struct is for dog
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
@@ -20,5 +21,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->age = age;
 		d->owner = owner;
 	}
-	/* Copyright © 2022 baccrie */
+
 }
