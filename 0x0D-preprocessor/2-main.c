379918ce61ea3b0f097d3d764d0826187f41434d
baccrie <bakarerilwan1@gmail.com>
2022-04-11 16:01:22 +0100

    Fixed 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,14 +3,16 @@
 /**
  * main - Entry point
  * Return: 0 or 1
+ * Copyright © 2022 baccrie
  */
 
 #ifndef NAME
 #define NAME __FILE__
 #endif
 
-int main()
+int main(void)
 {
 	printf("%s\n", NAME);
 	return (0);
+	/* Copyright © 2022 baccrie */
 }
