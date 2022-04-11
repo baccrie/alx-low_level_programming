c1bb55b3baeb3cca3a132dd61422a6911e7c5597
baccrie <bakarerilwan1@gmail.com>
2022-04-11 14:03:57 +0100

    Fixed

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,8 +11,15 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == null)
+	{
+		return;
+	}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 	/* Copyright © 2022 baccrie */
 }
