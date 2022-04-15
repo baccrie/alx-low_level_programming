859acf7bff1c2dd80e69436397c77db3d38ec17b
baccrie <bakarerilwan1@gmail.com>
2022-04-15 18:47:28 +0100

    Fixed

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	/* Declaring the variables using unsigned long because */
 	/* it has more memory and can accommodate the big fibonnaci numbers */
-	long sum, sum1, sum2;
+	long unsigned  sum, sum1, sum2;
 	short int count;
 
 	sum2 = 1;
