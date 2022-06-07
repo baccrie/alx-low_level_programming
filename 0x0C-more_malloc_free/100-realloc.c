752365f26391cd6ae56f94effba02239f9efc715
baccrie <bakarerilwan1@gmail.com>
2022-06-07 14:46:06 +0100

    Enipe

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -11,18 +11,18 @@
  * if malloc fails, returns NULL.
  */
 
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-        if (old_size == new_size)
-                return (ptr);
-        if (new_size == 0 && ptr != NULL)
-        {
-                free (ptr);
-                return (NULL);
-        }
-        if (ptr == NULL)
-                ptr = malloc(new_size);
-        ptr = malloc(new_size);
-        return (ptr);
+	if (old_size == new_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free (ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+	free (ptr);
+	ptr = malloc(new_size);
+	return (ptr);
 }
