c2236dfc9d15b3b9a25e0689f09135fee86462b6
baccrie <bakarerilwan1@gmail.com>
2022-07-02 13:47:16 +0100

    Task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "lists.h"
+
+/**
+ * pop_listint - A Func
+ * @head: double pointer
+ * Return: int
+ * Copyright © 2022 baccrie
+ */
+
+int pop_listint(listint_t **head)
+{
+	int value;
+	listint_t *tmp;
+
+	tmp = (*head)->next;
+	if (*head == NULL)
+		return (0);
+	value = (*head)->n;
+	free(*head);
+	*head = tmp;
+
+	return (value);
+}
