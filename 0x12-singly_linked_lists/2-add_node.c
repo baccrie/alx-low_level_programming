174b136f5fe80d6253fab533db4d337154782fd5
baccrie <bakarerilwan1@gmail.com>
2022-07-02 09:46:51 +0100

    Task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "lists"
+#include "stdlib.h"
+
+/**
+ * add_node - add nodes to list
+ * @head: a double pointer pointing to a list head
+ * @str: node data
+ * Return: pointer to list head
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *tmp;
+
+	tmp = malloc(sizeof(tmp));
+	if (tmp == NULL)
+		return;
+	_strcpy(tmp->str, str);
+	tmp->next = *head;
+	*head = tmp;
+
+	return (*head);
+}
