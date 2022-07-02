8e518f78514a23cf8d695a194a2281bd7b772afb
baccrie <bakarerilwan1@gmail.com>
2022-07-02 13:23:51 +0100

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list
+ * @head: double pointer to head node
+ * Return: nothing
+ * Copyright © 2022 baccrie
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	tmp = *head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		free(*head);
+		*head = tmp;
+	}
+	free(*head);
+
+}
