f2ccabb531cd82b23eb84fbffba8d96b7b1a10c3
baccrie <bakarerilwan1@gmail.com>
2022-07-02 09:29:02 +0100

    Fixed

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * list_len - function that counts nodes
+ * @h: pointer to first node
+ * Return: no of nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t size;
+	const list_t *tmp;
+
+	tmp = h;
+	size = 0;
+	while (tmp != NULL)
+	{
+		size++;
+		tmp = tmp->next;
+	}
+
+	return (size);
+}
