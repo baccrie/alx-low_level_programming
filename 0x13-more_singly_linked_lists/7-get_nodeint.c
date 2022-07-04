1ddcf480b32cecaf45874a7104dd74f0570d14ad
baccrie <bakarerilwan1@gmail.com>
2022-07-04 14:37:13 +0100

    Fix 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -23,7 +23,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		tmp = tmp->next;
 	}
 	tmp = head;
-	if (index > size || size < 0)
+	if ((index > size) || (index < 0))
 		return (NULL);
 	while (i< index)
 	{
