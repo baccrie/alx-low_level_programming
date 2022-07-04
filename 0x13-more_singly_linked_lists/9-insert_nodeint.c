fb1a29815f0ec4572c0cf20a2bd34bc15c1626cf
baccrie <bakarerilwan1@gmail.com>
2022-07-04 15:29:50 +0100

    Task 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+#include "lists.h"
+
+
+/**
+ * insert_nodeint_at_index - add node at index
+ * @head: head of node
+ * @n: node data
+ * @idx: index to add node
+ * Return: NULL or pointer
+ * Copyright © baccrie #Doing the hard things ALX 22
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *tmp, *ptr;
+	unsigned int size, i;
+
+	size = 0;
+	i = 0;
+	tmp = malloc(sizeof(listint_t));
+	ptr = *head;
+	if (tmp == NULL)
+		return (NULL);
+	tmp->n = n;
+	while (ptr != NULL)
+	{
+		ptr = ptr->next;
+		size++;
+	}
+	ptr = *head;
+	if (idx > size)
+	{
+		return (NULL);
+	}
+
+	while (i < (idx - 1))
+	{
+		ptr = ptr->next;
+		i++;
+	}
+	tmp->next = ptr->next;
+	ptr->next = tmp;
+
+	return (tmp);
+}
+
+
