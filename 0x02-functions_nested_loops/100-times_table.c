7e70876ffd1bb2963a449b91c9a4955a3c2d1a48
baccrie <bakarerilwan1@gmail.com>
2022-07-06 21:23:32 +0100

    Task 1-Advanced simple and straight

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
  * print_times_table - prints the times
@@ -13,44 +13,27 @@
 
 void print_times_table(int n)
 {
-	int row, column, product;
+	int i, j, ans;
 
-	if (n >= 0 && n < 15)
+	i = j = 0;
+	ans = 1;
+	while (i <= n)
 	{
-		for (row = 0; row <= n; row++)
+		j = 0;
+		if (n > 15 || n < 0)
+			return;
+		while (j <= n)
 		{
-			for (column = 0; column <= n; column++)
-			{
-				product = row * column;
-
-				if (column == 0)
-					_putchar('0');
-				else if (product < 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product % 10 + '0');
-				}
-				else if (product >= 10 && product < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-				else if (product > 99 && product < 1000)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(product / 100 + '0');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
-				}
-			}
-			_putchar('\n');
+			ans = i * j;
+			if (j != 0)
+				printf("%3d", ans);
+			else
+				printf("%d", ans);
+			if (j != n)
+				printf(",");
+			j++;
 		}
+		putchar('\n');
+		i++;
 	}
 }
