1817c759fff3f6ed42db53f4d96023ce0d4fc770
baccrie <bakarerilwan1@gmail.com>
2022-07-17 09:28:35 +0100

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,24 +12,15 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j, k;
+	int i, j;
 
 	i = 0;
 	j = 0;
-	k = 0;
 	while (dest[i])
 	{
 		i++;
 	}
-	while (src[k])
-	{
-		k++;
-	}
-	if (n > k)
-	{
-		n = k;
-	}
-	while (j < n)
+	while (j < n && src[j] != 0)
 	{
 		dest[i] = src[j];
 		i++;
