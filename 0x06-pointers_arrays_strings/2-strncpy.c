f2ce13a7e5eee92325fcf8de5c9bfed9e2308274
baccrie <bakarerilwan1@gmail.com>
2022-07-17 10:01:52 +0100

    Task 3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
  * _strncpy
@@ -11,19 +11,64 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i, j, k;
 
-	i = 0;
-	while (i < n && *(src + i))
-	{
-		*(dest + i) = *(src + i);
+	i = j = k = 0;
+	while (dest[j])
+		j++;
+	while (src[i])
 		i++;
-	}
-	while (i < n)
+	while (src[k])
 	{
-		*(dest + i)  = '\0';
-		i++;
+		dest[j] = src[k];
+		j++;
+		k++;
 	}
+	if (n > i)
+	{
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	dest[j] = '\0';
+
 	return (dest);
-	/* Copyright © 2022 baccrie */
+
 }
+
+int main(void)
+{
+    char s1[98];
+    char *ptr;
+    int i;
+
+    for (i = 0; i < 98 - 1; i++)
+    {
+        s1[i] = '*';
+    }
+    s1[i] = '\0';
+    printf("%s\n", s1);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
+    printf("%s\n", s1);
+    printf("%s\n", ptr);
+    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
+    printf("%s", s1);
+    printf("%s", ptr);
+    for (i = 0; i < 98; i++)
+    {
+        if (i % 10)
+        {
+            printf(" ");
+        }
+        if (!(i % 10) && i)
+        {
+            printf("\n");
+        }
+        printf("0x%02x", s1[i]);
+    }
+    printf("\n");
+    return (0);
+}
+
--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "stdio.h"
 
 /**
  * _strcmp -  compares two strings
@@ -12,11 +12,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	i = 0;
-	while (*(s1 + i) && *(s2 + i) && *(s1 + i) == *(s2 + i))
+	if (s1 == s2)
 	{
-		i++;
+		return (0);
 	}
-	return (s1[i] - s2[i]);
-	/* Copyright © 2022 baccrie */
+
+	return (s1[0] - s2[0]);
 }
