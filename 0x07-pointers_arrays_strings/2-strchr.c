c3eef96172d8a432043b1e99b38ec22eeee9ec62
baccrie <bakarerilwan1@gmail.com>
2022-07-24 17:51:13 +0100

    Task

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,27 +5,14 @@
  * @s: String
  * @c: Character to search
  * Return: Pointer to the first occurrence of the character c in the string s
- **/
+ */
 
 char *_strchr(char *s, char c)
 {
-
-	while (*s != '\0')
+	while (*s != c)
 	{
-		if (*s == c)
-		{
-			return (s);
-		}
-
-		++s;
+		s++;
 	}
 
-	if (*s == c)
-	{
-		return (s);
-
-	}
-
-
-	return (0);
+	return (s);
 }
