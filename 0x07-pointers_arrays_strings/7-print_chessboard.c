4b4867fdf63260d907386bf930ae49433e846d1e
baccrie <bakarerilwan1@gmail.com>
2022-07-25 13:57:41 +0100

    Task 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,3 +1,5 @@
+#include "stdio.h"
+#include "string.h"
 #include "main.h"
 
 /**
@@ -12,7 +14,7 @@ void print_chessboard(char (*a)[8])
 	i = 0;
 	while (i < 8)
 	{
-		j = 0;
+		j = sizeof((*a)[i]) / sizeof(char);
 		while (j < 8)
 		{
 			_putchar (a[i][j]);
