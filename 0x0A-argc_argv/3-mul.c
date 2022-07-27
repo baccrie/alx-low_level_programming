8c7e05e6d451f12f104fd9156bcac38f77c6c201
baccrie <bakarerilwan1@gmail.com>
2022-07-28 00:36:29 +0100

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,12 +18,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
 	a = atoi(argv[1]);
 	b = atoi(argv[2]);
 	result = a * b;
 	printf("%d\n", result);
 	return (0);
-	}
 }
