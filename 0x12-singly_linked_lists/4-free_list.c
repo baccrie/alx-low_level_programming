622d115c742f027fd625ce1317a182ef35e87b0d
baccrie <bakarerilwan1@gmail.com>
2022-08-19 00:46:29 +0100

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include "string.h"
+#include "stdlib.h"
+
+/**
+ * free_list - frees list
+ * @head: head node
+ * Return: head node
+ */
+
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		free(head->str);
+		free(head);
+	}
+}
