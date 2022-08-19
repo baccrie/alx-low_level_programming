24894f810b806f070dc0ac598f3ae0c57e7cf8fd
baccrie <bakarerilwan1@gmail.com>
2022-08-19 21:06:46 +0100

    Task 4....

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -21,26 +21,3 @@ void free_list(list_t *head)
 		head = tmp;
 	}
 }
-/**
- * free_list -  free a list
- * @head: pointer
- * Return: void
-
-void free_list(list_t *head)
-{
-	list_t *temp;
-
-	if (head == NULL)
-		return;
-
-	while (head->next != NULL)
-	{
-		temp = head->next;
-		free(head->str);
-		free(head);
-		head = temp;
-	}
-
-	free(head->str);
-	free(head);
-}*/
