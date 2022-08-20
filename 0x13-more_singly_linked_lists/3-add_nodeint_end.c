ac21b8a4d378136676ea1b3553b9b0a8a1351d2d
baccrie <bakarerilwan1@gmail.com>
2022-08-20 21:33:31 +0100

    Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,3 +9,21 @@
  * Return: pointer or null
  * Copyright © 2022 baccrie
  */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr, *tmp;
+
+	tmp = *head;
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+		return (NULL);
+	ptr->n = n;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+	}
+	ptr->next = NULL;
+	tmp->next = ptr;
+	return (ptr);
+}
