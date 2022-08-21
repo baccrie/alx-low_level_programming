ba4670b98a1ce9d6e7e0e61a2274b662f06253d8
baccrie <bakarerilwan1@gmail.com>
2022-08-21 14:50:37 +0100

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,3 +9,29 @@
  * @n: node data
  * @idx: index to add node
  * Return: NULL or pointer
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *tmp, *prev;
+	size_t i;
+
+	i = 0;
+	prev = *head;
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	tmp = malloc(sizeof(listint_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->n = n;
+	while (i < (idx - 1) & prev->next != NULL)
+	{
+		prev = (prev)->next;
+		i++;
+	}
+
+	tmp->next = prev->next;
+	prev->next = tmp;
+	return (*head);
+}
