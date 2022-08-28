a420e635474923283ed4dc1cef23fcd4b4895df0
baccrie <bakarerilwan1@gmail.com>
2022-08-28 08:29:54 +0100

    Task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,22 +9,14 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	unsigned int i;
 
-	count = 0;
-
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
-	{
+	i = 0;
+	while (h != NULL) {
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		i++;
 	}
 
-	return (count);
+	return (i);
 }
