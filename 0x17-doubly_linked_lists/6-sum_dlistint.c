033733c1dfd125ed3ac8cf27235096eaaad2f5ed
baccrie <bakarerilwan1@gmail.com>
2022-08-28 10:06:24 +0100

    Task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,3 +9,18 @@
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int i;
+	dlistint_t *ptr;
+
+	ptr = head;
+	i = 0;
+	if (head == NULL) {
+		return (0);
+	}
+	while (ptr != NULL) {
+		i += ptr->n;
+		ptr = ptr->next;
+	}
+
+	return (i);
+}
