1ee34efc8e751bf06699ce7346c4959102cb157b
baccrie <bakarerilwan1@gmail.com>
2022-08-29 19:10:39 +0100

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,3 +9,34 @@
  * @n: value of the new node
  * Return: the address of the new node, or NULL if it failed
  */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *ptr, *ptr1;
+	size_t i;
+
+	i = 0;
+	ptr1 = *h;
+	ptr = malloc(sizeof(dlistint_t));
+	ptr->n = n;
+	ptr->next = NULL;
+	ptr->prev = NULL;
+	if (ptr == NULL)
+		return (NULL);
+	if (*h == NULL || h == NULL)
+		return NULL;
+	if (idx == 0) {
+		ptr->next = (*h)->next;
+		ptr->prev = NULL;
+		*h = ptr;
+		return (*h);
+	}
+	while (i < (idx - 1)) {
+		ptr1 = ptr1->next;
+		i++;
+	}
+	ptr->next = ptr1->next;
+	ptr1->next = ptr;
+	ptr->prev = ptr1;
+	return (*h);
+}
