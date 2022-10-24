f7cf5e0164e98f70897062745092844c1a816360
baccrie <bakarerilwan1@gmail.com>
2022-10-24 21:24:42 +0100

    Task 0.0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,18 +8,16 @@
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h) {
-	const dlistint_t *ptr;
 	size_t nodes;
 
-	ptr = h;
 	nodes = 0;
 	if (h == NULL) {
 		return (nodes);
 	}
-	while (ptr != NULL) {
+	while (h != NULL) {
 		nodes += 1;
-		printf("%d\n", ptr->n);
-		ptr = ptr->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 
 	return (nodes);
