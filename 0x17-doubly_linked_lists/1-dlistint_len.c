c9183dfd7d9422fcd39787860e88be67a946a7c3
baccrie <bakarerilwan1@gmail.com>
2022-10-24 21:45:34 +0100

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,21 +1,25 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in
- * a double linked list
- *
+ * dlistint_len - returns the no of elements in a list
  * @h: head of the list
  * Return: the number of nodes
+ * copyright baccrie © 2022
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t i;
+	size_t nodes;
+	const dlistint_t *ptr;
 
-	i = 0;
-	while (h != NULL) {
-		i++;
-		h = h->next;
+	nodes = 0;
+	ptr = h;
+	if (ptr == NULL)
+		return (nodes);
+	while (ptr != NULL)
+	{
+		nodes += 1;
+		ptr = ptr->next;
 	}
 
-	return (i);
+	return (nodes);
 }
